239ce520306d11b5b822527445c38a25139dcbe0
Emil Mladenov <moonclash@Emils-MacBook-Pro-2.local>
2025-05-30 21:22:38 +0300

    add addon

---
+++ addon.js
@@ -0,0 +1,33 @@
+const { addonBuilder } = require('stremio-addon-sdk');
+const scrapeWatchlist = require('./scraper');
+
+const manifest = {
+  id: 'org.you.letterboxd.watchlist',
+  version: '1.0.0',
+  name: 'Letterboxd Watchlist',
+  description: 'Displays your Letterboxd watchlist in Stremio',
+  resources: ['catalog'],
+  types: ['movie'],
+  catalogs: [
+    {
+      type: 'movie',
+      id: 'letterboxd-watchlist'
+    }
+  ]
+};
+
+const builder = new addonBuilder(manifest);
+
+builder.defineCatalogHandler(async () => {
+  const movies = await scrapeWatchlist('your_username');
+  
+  const metas = movies.map(movie => ({
+    name: movie.title,
+    type: 'movie',
+    poster: movie.posterUrl,
+  }));
+
+  return { metas };
+});
+
+module.exports = builder.getInterface();
---
+++ scraper.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+function constructFilmPoster(filmId, filmSlug) {
+    const splitId = filmId.split('');
+    return `https://a.ltrbxd.com/resized/film-poster/${splitId.join('/')}/${filmId}-${filmSlug}-0-250-0-375-crop.jpg`;
+}
+
+async function scrapeWatchList(username) {
+    const url = `https://letterboxd.com/${username}/watchlist/`;
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+    const films = [];
+    $('.poster-container').each((index, element) => {
+        const title = $(element).find('.film-poster').find('img').attr('alt');
+        const filmId = $(element).find('.film-poster').attr('data-film-id');
+        const filmSlug = $(element).find('.film-poster').attr('data-film-slug');
+        const posterUrl = constructFilmPoster(filmId, filmSlug);
+        films.push({
+            title,
+            posterUrl,
+        });
+    });
+    return films;
+}
+module.exports = scrapeWatchList;
